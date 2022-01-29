754a80ace8243cbcb3e146c16ce3ab1b2116e577
madams10m <54149526+madams10m@users.noreply.github.com>
2022-01-29 16:18:04 -0600

    adding variable to adminsshKey

--- terraform/arkServerVM.tf
+++ terraform/arkServerVM.tf
@@ -75,7 +75,7 @@ resource "azurerm_linux_virtual_machine" "arkVM" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = var.myPublicKey
   }
 
   os_disk {

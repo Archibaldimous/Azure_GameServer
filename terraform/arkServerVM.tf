6061440b6154572101854af5fb6532f85ce41ad9
madams10m <54149526+madams10m@users.noreply.github.com>
2022-01-29 17:32:00 -0600

    adding nsg to nic

--- terraform/arkServerVM.tf
+++ terraform/arkServerVM.tf
@@ -71,6 +71,7 @@ resource "azurerm_network_interface" "arkVM1NIC" {
   ]
   location            = azurerm_resource_group.arkRG.location
   resource_group_name = azurerm_resource_group.arkRG.name
+  network_security_group_id = azurerm_network_security_group.NSG1.id
 
 
   ip_configuration {

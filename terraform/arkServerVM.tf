c7a150d9f9a5b5bb6a3f5ae2a873ce8022186516
madams10m <54149526+madams10m@users.noreply.github.com>
2022-01-29 16:58:33 -0600

    upping machine size

--- terraform/arkServerVM.tf
+++ terraform/arkServerVM.tf
@@ -53,7 +53,7 @@ resource "azurerm_linux_virtual_machine" "arkVM" {
   name                = "arkVM1-ragnarok"
   resource_group_name = azurerm_resource_group.arkRG.name
   location            = azurerm_resource_group.arkRG.location
-  size                = "Standard_B2s"
+  size                = "Standard_D4s_v3"
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.arkVM1NIC.id

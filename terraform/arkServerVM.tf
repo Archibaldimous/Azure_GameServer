2075b0c68a7a52a2065833549e426542d2059bc6
Archibaldimous <masona4295@gmail.com>
2022-10-07 17:06:01 -0500

    updating from ark to minecraft, all my homies hate ark

--- terraform/arkServerVM.tf
+++ terraform/arkServerVM.tf
@@ -16,30 +16,30 @@ variable "myPublicKey" {
 
 }
 
-resource "azurerm_resource_group" "arkRG" {
-  name     = "arkServerRG"
+resource "azurerm_resource_group" "MineCraftRG" {
+  name     = "MineCraftServerRG"
   location = "South Central US"
 }
 
 resource "azurerm_virtual_network" "Vnet1" {
-  name                = "arkVnet1"
+  name                = "MineCraftVnet1"
   address_space       = ["10.0.0.0/18"]
-  location            = azurerm_resource_group.arkRG.location
-  resource_group_name = azurerm_resource_group.arkRG.name
+  location            = azurerm_resource_group.MineCraftRG.location
+  resource_group_name = azurerm_resource_group.MineCraftRG.name
 }
 
 resource "azurerm_subnet" "Snet1" {
   name                 = "Snet1"
-  resource_group_name  = azurerm_resource_group.arkRG.name
+  resource_group_name  = azurerm_resource_group.MineCraftRG.name
   virtual_network_name = azurerm_virtual_network.Vnet1.name
   address_prefixes     = ["10.0.0.0/18"]
 }
 
 
 resource "azurerm_network_security_group" "NSG1" {
-  name                = "arkVMNSG"
-  location            = azurerm_resource_group.arkRG.location
-  resource_group_name = azurerm_resource_group.arkRG.name
+  name                = "MineCraftVMNSG"
+  location            = azurerm_resource_group.MineCraftRG.location
+  resource_group_name = azurerm_resource_group.MineCraftRG.name
 
   security_rule {
     name                       = "allowSSH"
@@ -49,52 +49,52 @@ resource "azurerm_network_security_group" "NSG1" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefixes    = ["70.191.107.0/24", "98.160.98.0/24", "70.185.205.0/24"]
+    source_address_prefixes    = ["70.191.107.0/24", "98.160.98.0/24", "70.185.205.0/24", "162.207.79.0/24"]
     destination_address_prefix = "*"
   }
   security_rule {
-    name                       = "allowArkTraffic"
+    name                       = "allowMineCraftTraffic"
     priority                   = 105
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
-    destination_port_ranges    = ["7777", "7778", "27015", "443", "80","27020"]
+    destination_port_ranges    = ["7777", "7778", "27015", "443", "80","27020","25565"]
     source_address_prefix      = "Internet"
     destination_address_prefix = "*"
   }
 }
-resource "azurerm_network_interface" "arkVM1NIC" {
-  name = "arkVM1NIC"
+resource "azurerm_network_interface" "MineCraftVM1NIC" {
+  name = "MineCraftVM1NIC"
   depends_on = [
-    azurerm_public_ip.arkVM1PIP
+    azurerm_public_ip.MineCraftVM1PIP
   ]
-  location                  = azurerm_resource_group.arkRG.location
-  resource_group_name       = azurerm_resource_group.arkRG.name
+  location                  = azurerm_resource_group.MineCraftRG.location
+  resource_group_name       = azurerm_resource_group.MineCraftRG.name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.Snet1.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "10.0.0.69"
-    public_ip_address_id          = azurerm_public_ip.arkVM1PIP.id
+    public_ip_address_id          = azurerm_public_ip.MineCraftVM1PIP.id
   }
 }
 
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.arkVM1NIC.id
+  network_interface_id      = azurerm_network_interface.MineCraftVM1NIC.id
   network_security_group_id = azurerm_network_security_group.NSG1.id
 }
 
 
-resource "azurerm_linux_virtual_machine" "arkVM" {
-  name                = "arkVM1-ragnarok"
-  resource_group_name = azurerm_resource_group.arkRG.name
-  location            = azurerm_resource_group.arkRG.location
+resource "azurerm_linux_virtual_machine" "MineCraftVM" {
+  name                = "MineCraftVM1-ragnarok"
+  resource_group_name = azurerm_resource_group.MineCraftRG.name
+  location            = azurerm_resource_group.MineCraftRG.location
   size                = "Standard_D4s_v3"
   admin_username      = "mason"
   network_interface_ids = [
-    azurerm_network_interface.arkVM1NIC.id
+    azurerm_network_interface.MineCraftVM1NIC.id
   ]
 
   admin_ssh_key {
@@ -115,17 +115,17 @@ resource "azurerm_linux_virtual_machine" "arkVM" {
   }
 }
 
-resource "azurerm_public_ip" "arkVM1PIP" {
-  name                = "arkPIP1"
-  location            = azurerm_resource_group.arkRG.location
-  resource_group_name = azurerm_resource_group.arkRG.name
+resource "azurerm_public_ip" "MineCraftVM1PIP" {
+  name                = "MineCraftPIP1"
+  location            = azurerm_resource_group.MineCraftRG.location
+  resource_group_name = azurerm_resource_group.MineCraftRG.name
   allocation_method   = "Static"
 }
 
 resource "azurerm_managed_disk" "dataDisk1" {
-  name                 = "arkDataDisk1"
-  location             = azurerm_resource_group.arkRG.location
-  resource_group_name  = azurerm_resource_group.arkRG.name
+  name                 = "MineCraftDataDisk1"
+  location             = azurerm_resource_group.MineCraftRG.location
+  resource_group_name  = azurerm_resource_group.MineCraftRG.name
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = "256"
@@ -141,11 +141,11 @@ resource "azurerm_managed_disk" "dataDisk1" {
 */
 resource "azurerm_virtual_machine_data_disk_attachment" "vm1DataDiskAttach" {
   depends_on = [
-    azurerm_linux_virtual_machine.arkVM,
+    azurerm_linux_virtual_machine.MineCraftVM,
     azurerm_managed_disk.dataDisk1
   ]
   managed_disk_id    = azurerm_managed_disk.dataDisk1.id
-  virtual_machine_id = azurerm_linux_virtual_machine.arkVM.id
+  virtual_machine_id = azurerm_linux_virtual_machine.MineCraftVM.id
   lun                = "4"
   caching            = "ReadWrite"
 }
